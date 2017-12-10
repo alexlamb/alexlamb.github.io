6306587277d591d1d23b89628c5c8b8eff1cfe57
Alex Lamb <alex.lamb@gmail.com>
2017-12-10 12:55:32 -0800

    Added trial wix enabled child pages

---
+++ js/tChild1.js
@@ -0,0 +1,268 @@
+window.onload = function() {
+  window.onmessage = (event) => {
+    console.log("C1 message received from parent");
+    if (event.data) {
+      // console.log(JSON.stringify(event));
+      // console.log(JSON.stringify(event.data));
+
+      if (event.data.action === "Init") {
+          initUI(event.data.content);
+      } else if (event.data.action === "Freeze") {
+          console.log("C1 Freeze request");
+          setFrozen(true);
+      } else if (event.data.action === "Unfreeze") {
+          console.log("C1 Unfreeze request");
+          setFrozen(false);
+      } else if (event.data.action === "Remove") {
+          removeIngredient(event.data.content);
+      }
+    }
+  }
+}
+
+const assets = "https://alexlamb.github.io/js/assets/"
+// const assets = "assets/"
+const leftChev = "chevron-left.png";
+const rightChev = "chevron-right.png";
+const width = 1000;
+const height = 130;
+const optionsWidth = width - 100;
+const optionOffset = 45;
+const itemsPerPage = 6;
+const itemWidth = 150;
+const itemHeight = 100;
+
+var optionButtons = [];
+var optionBorders = [];
+var optionRects = [];
+var optionShades = [];
+var buttons;
+var state;
+var buttonData;
+var frozen = false;
+var shift = 0;
+var pages = 1;
+var page = 0;
+// var selected = 0;
+
+var leftChevron;
+var rightChevron;
+
+function initUI(data) {
+    buttonData = data;
+    state = new Array(data.length);
+    state.fill(0);
+
+    pages = Math.floor((data.length * itemWidth) / optionsWidth);
+    console.log("C1 pages:"+pages);
+
+    frozen = false;
+
+    var buttonSideW = itemWidth * 3 / 4;
+    var buttonSideH = itemHeight * 3 / 4;
+    var buttonRimW = (itemWidth - buttonSideW) / 2;
+    var buttonRimH = (itemHeight - buttonSideH) / 2;
+    var borderSideW = itemWidth * 5 / 6;
+    var borderSideH = itemHeight * 5 / 6;
+    var borderRimW = (itemWidth - borderSideW) / 2;
+    var borderRimH = (itemHeight - borderSideH) / 2;
+
+    draw = SVG('svgCanvas').size(width, height);
+    draw.rect(width, height).fill("#F0F0F0");
+    buttons = draw.group();
+    edges = draw.group();
+
+    for (let i = 0; i < data.length; i++) {
+        var item = data[i];
+
+        let optionRect = buttons.rect(borderSideW,borderSideH).fill('none');
+        optionRect.move(borderRimW+(optionOffset+i*itemWidth),borderRimH);
+        optionRect.radius(15);
+        optionRects.push(optionRect);
+
+        let optionShade = buttons.rect(borderSideW,borderSideH).fill('none');
+        optionShade.move(borderRimW+(optionOffset+i*itemWidth),borderRimH);
+        optionShade.radius(15);
+        optionShades.push(optionShade);
+
+        let optionButton = buttons.image(assets+data[i].image,buttonSideW,buttonSideH);
+        optionButtons.push(optionButton);
+        optionButton.move(buttonRimW+(optionOffset+i*itemWidth),buttonRimH);
+
+        optionButton.style('cursor', 'pointer');
+        optionButton.click(function() {selectOption(i)});
+        optionButton.mouseover(function() {highlightOption(i)});
+        optionButton.mouseout(function() {fadeOption(i)});
+
+        let optionBorder = buttons.rect(borderSideW,borderSideH);
+        optionBorders.push(optionBorder);
+        optionBorder.fill('none').stroke({color: 'Silver', width: 1});
+        optionBorder.move(borderRimW+(optionOffset+i*itemWidth),borderRimH);
+        optionBorder.radius(15);
+
+        let optionText = buttons.text(data[i].name);
+        let length = optionText.length();
+        let adjust = (borderSideW - length)/2;
+        optionText.move(borderRimW+(optionOffset+i*itemWidth)+adjust,borderRimH+borderSideH+10);
+        optionText.fill('Black');
+    }
+
+    leftEdge = edges.rect(optionOffset, height);
+    leftEdge.fill('#F0F0F0');
+    leftEdge.click(function() {pageLeft()});
+    // leftIm = edges.image(assets+leftChev,optionOffset, height);
+    // leftIm.click(function() {pageLeft()});
+
+    rightEdge = edges.rect(optionOffset, height).move(width-(optionOffset),0);
+    rightEdge.fill('#F0F0F0');
+    rightEdge.click(function() {pageRight()});
+
+    // rimX = edges.rect(width,height);
+    // rimX.fill('none').stroke('Silver');
+
+    const chevronL = optionOffset/2 - 10;
+    const chevronR = optionOffset/2 + 10;
+    const chevronT = height/2 - 18;
+    const chevronB = height/2 + 18;
+
+    let leftPathString = "M "+chevronR+" "+chevronT+" ";
+    leftPathString += "L "+chevronL+" "+(height/2)+" ";
+    leftPathString += "L "+chevronR+" "+chevronB+" ";
+    leftChevron = edges.path(leftPathString);
+    let chevStroke = {
+        width:4,
+        color:'Gray',
+        linecap:'round'
+    };
+
+    leftChevron.fill('none').stroke(chevStroke);
+
+    let rightPathString = "M "+chevronL+" "+chevronT+" ";
+    rightPathString += "L "+chevronR+" "+(height/2)+" ";
+    rightPathString += "L "+chevronL+" "+chevronB+" ";
+    rightChevron = edges.path(rightPathString);
+    rightChevron.fill('none').stroke(chevStroke);
+    rightChevron.dmove(width-(optionOffset+5),0);
+
+    // updateChevrons();
+}
+
+function highlightOption(i) {
+    console.log("C1 highlight:"+i);
+    if (!frozen || state[i] > 0) {
+        optionShades[i].animate(5).fill('White');
+    }
+}
+
+function fadeOption(i) {
+    console.log("C1 fade:"+i+" frozen:"+frozen+" state:"+state[i]+" func:"+(!frozen || state[i] > 1));
+    if (!frozen || state[i] > 0) {
+        console.log("C1 fading");
+        optionShades[i].animate(5).fill('none');
+    }
+}
+
+function pageLeft() {
+    if (page > 0) {
+        page--;
+        buttons.animate(250).dmove(itemWidth*itemsPerPage,0);
+        // updateChevrons();
+    } else {
+        page = pages - 1;
+        buttons.dmove(-itemWidth*itemsPerPage*pages,0);
+        buttons.animate(250).dmove(itemWidth*itemsPerPage,0);
+    }
+    console.log("C1 page:"+page+" shift:"+buttons.x());
+}
+
+function pageRight() {
+    if (page < pages-1) {
+        page++;
+        buttons.animate(250).dmove(-itemWidth*itemsPerPage,0);
+        // updateChevrons();
+    } else {
+        page = 0;
+        buttons.dmove(itemWidth*itemsPerPage*pages,0);
+        buttons.animate(250).dmove(-itemWidth*itemsPerPage,0);
+    }
+    console.log("C1 page:"+page+" shift:"+buttons.x());
+}
+
+// function updateChevrons() {
+//     console.log("page:"+page+" pages:"+pages);
+//     if (page == 0) {
+//         leftChevron.stroke('none');
+//     } else {
+//         leftChevron.stroke('Gray');
+//     }
+//     if (page == pages - 1) {
+//         rightChevron.stroke('none');
+//     } else {
+//         rightChevron.stroke('Gray');
+//     }
+// }
+
+function setFrozen(value) {
+    console.log("C1 setFrozen");
+    frozen = value;
+
+    for (let i = 0; i < state.length; i++) {
+        if (state[i] == 0) {
+            if (frozen) {
+                // optionBorders[i].stroke('DimGray');
+                optionRects[i].fill('DimGray');
+            } else {
+                // optionBorders[i].stroke('Silver');
+                optionRects[i].fill('none');
+            }
+        }
+    }
+}
+
+function selectOption(i) {
+
+    let sendMessage = false;
+
+    console.log("C1 selectOption:"+i);
+    // if (state[i] == 0 && !frozen && selected < 5) {
+    if (state[i] == 0 && !frozen) {
+        optionBorders[i].stroke({color:'#52009A', width:2});
+        optionRects[i].fill('#BAB6CA');
+        state[i] = 1;
+        sendMessage = true;
+        // selected++;
+        // if (selected >= 5) {
+        //     setFrozen(true);
+        // }
+
+    // } else if (state[i] > 0 && selected < 5) {
+    } else if (state[i] > 0) {
+        optionBorders[i].stroke({color:'Silver', width:1});
+        console.log("C1 Unfreezing");
+        state[i] = 0;
+        setFrozen(false);
+        sendMessage = true;
+        // selected--;
+    }
+
+    if (sendMessage) {
+        var message = {
+            action: "UpdateSelection",
+            selection: state
+        };
+        window.parent.postMessage(message,"*");
+    }
+    // console.log("Selected:"+selected);
+}
+
+function removeIngredient(data) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] == 0 && state[i] > 0) {
+
+            optionBorders[i].stroke('none');
+            console.log("C1 Unfreezing");
+            state[i] = 0;
+            setFrozen(false);
+        }
+    }
+}
---
+++ js/tChild2.js
@@ -0,0 +1,529 @@
+twindow.onload = function() {
+    // console.log("Started up");
+  window.onmessage = (event) => {
+    console.log("C2 message received from parent");
+    if (event.data) {
+      // console.log(JSON.stringify(event));
+      // console.log(JSON.stringify(event.data));
+
+      if (event.data.action === "Init") {
+          initUI(event.data.content);
+      } else {
+          updateSelection(event.data.content);
+      }
+    }
+  }
+}
+
+const assets = "https://alexlamb.github.io/js/assets/"
+// const arrowsFile = "arrows.png";
+const units = 12;
+const maxFlavors = 5;
+const initialAdd = 2;
+
+var optionData;
+var width;
+var height;
+var state;
+var buttons;
+var controls;
+var capacity;
+var helpText;
+var selected;
+
+function initUI(data) {
+    optionData = data;
+    state = new Array(data.length);
+    state.fill(0);
+
+    width = 1000;
+    height = 1100;
+
+    draw = SVG('svgCanvas').size(width, height);
+    controls = draw.group();
+    capacity = units;
+    selected = 0;
+
+    helpText = draw.text("");
+    helpText.font({family:'Helvetica', size:25});
+    helpText.move(200,0);
+    updateUI();
+}
+
+function updateSelection(selectionData) {
+    console.log("C2 updateSelection");
+
+    //Determine how many flavors are now selected
+    let nowSelected = 0;
+    let priorSelected = 0;
+    let maxSelected = 0;
+    let minSelected = units;
+
+    for (let i = 0; i < selectionData.length; i++) {
+        if (selectionData[i] > 0) nowSelected++;
+        if (state[i] > 0) priorSelected++;
+        if (state[i] > maxSelected) maxSelected = state[i];
+        if (state[i] > 0 && state[i] < minSelected) minSelected = state[i];
+    }
+
+    let action = "UpdateRecipe";
+
+    if (nowSelected == 0) {
+        //Just clear everything out
+        console.log("Clearing bottle");
+        for (let i = 0; i < selectionData.length; i++) {
+            state[i] = 0;
+        }
+    } else if (nowSelected == 1) {
+        //There's only one flavor and it's new
+        console.log("One flavor");
+        for (let i = 0; i < selectionData.length; i++) {
+            if (selectionData[i] > 0) {
+                state[i] = units;
+            } else {
+                state[i] = 0;
+            }
+        }
+    // } else if (nowSelected == 1 && priorSelected >= 1) {
+    //     //There's only one flavor but we used to have more
+    //     console.log("Down to one flavor");
+    //     for (let i = 0; i < selectionData.length; i++) {
+    //         if (selectionData[i] == 0) {
+    //             state[i] = 0;
+    //         }
+    //     }
+
+    // } else if (nowSelected == 2) {
+    //     if (capacity < initialAdd && priorSelected < 2) {
+    //         //Our recipe is already maxed and we're adding. So we balance.
+    //         console.log("Two full flavors. Balancing.");
+    //         for (let i = 0; i < selectionData.length; i++) {
+    //             if (selectionData[i] > 0 && state[i] > units/2) {
+    //                 state[i] -= units/2;
+    //             } else if (selectionData[i] > 0 && state[i] == 0) {
+    //                 state[i] = units/2;
+    //             }
+    //         }
+    //
+    //     } else if (capacity >= initialAdd && priorSelected < 2) {
+    //         //Our recipe not too full and we're adding.
+    //         console.log("Empty 2-recipe. Adding.");
+    //         for (let i = 0; i < selectionData.length; i++) {
+    //             if (selectionData[i] > 0 && state[i] == 0) {
+    //                 state[i] = capacity;
+    //             }
+    //         }
+    //     } else  {
+    //         //General case
+    //         console.log("General 2-case");
+    //         if (selectionData[i] > 0 && state[i] == 0) {
+    //             state[i] = initialAdd;
+    //         } else if (selectionData[i] == 0 && state[i] > 0) {
+    //             state[i] = 0;
+    //         }
+    //     }
+    //
+    } else if (nowSelected <= maxFlavors) {
+
+        let frac = units / nowSelected;
+        let fair = Math.floor(frac);
+        let even = false;
+        if (frac == fair) even = true;
+
+        console.log("min:"+minSelected+" max:"+maxSelected+" frac:"+frac+"fair:"+fair+" even:"+even);
+
+        if (capacity == 0 && minSelected == maxSelected && even) {
+            //We have our ideal case
+            console.log("ideal case");
+            for (let i = 0; i < selectionData.length; i++) {
+                if (selectionData[i] > 0) {
+                    state[i] = fair;
+                } else if (selectionData[i] == 0 && state[i] > 0) {
+                    state[i] = 0;
+                }
+            }
+        }
+
+        else if (capacity == 0 && minSelected == maxSelected && !even) {
+            //We have a full even bottle but are going to end up with asymmetry
+            let newAmount = priorSelected;
+
+            console.log("full with asymmetry");
+            for (let i = 0; i < selectionData.length; i++) {
+                if (selectionData[i] > 0 && state[i] == 0) {
+                    state[i] = newAmount;
+                } else if (selectionData[i] > 0 && state[i] > 0) {
+                    state[i]--;
+                } else if (selectionData[i] == 0 && state[i] > 0) {
+                    state[i] = 0;
+                }
+            }
+        }
+
+        else if (capacity == 0 && nowSelected > priorSelected) {
+            //We remove from flavors until we have covered the initial add
+            console.log("removing to initial add");
+            removed = 0;
+            for (let i = 0; i < selectionData.length; i++) {
+                if (selectionData[i] > 0 && state[i] == 0) {
+                    state[i] = initialAdd;
+                } else if (selectionData[i] > 0 && state[i] > 1 && removed < initialAdd) {
+                    state[i]--;
+                    removed++;
+                } else if (selectionData[i] == 0 && state[i] > 0) {
+                    state[i] = 0;
+                }
+            }
+        }
+
+        else if (capacity >= minSelected) {
+            //We can assign the new flavor the minimum current
+            console.log("assigning minimum current");
+            for (let i = 0; i < selectionData.length; i++) {
+                if (selectionData[i] > 0 && state[i] == 0) {
+                    state[i] = minSelected;
+                } else if (selectionData[i] == 0 && state[i] > 0) {
+                    state[i] = 0;
+                }
+            }
+        }
+
+        else  {
+            //We assign remaining capacity to the new flavor
+            console.log("assigning remaining capacity");
+            let newAmount = capacity;
+
+            for (let i = 0; i < selectionData.length; i++) {
+                if (selectionData[i] > 0 && state[i] == 0) {
+                    state[i] = capacity;
+                } else if (selectionData[i] == 0 && state[i] > 0) {
+                    state[i] = 0;
+                }
+            }
+        }
+
+        // if (capacity < initialAdd && priorSelected < nowSelected) {
+        //     //Flavors are increasing but we have limited capacity.
+        //     //We have to move some ingredients around.
+        //
+        //     if (maxSelected <= initialAdd) {
+        //         //Flavors are really spread out so we have to balance.
+        //         console.log("Full n-recipe. Balancing.");
+        //         let subtracted = 0;
+        //         for (let i = 0; i < selectionData.length; i++) {
+        //             if (selectionData[i] > 0 && state[i] == 0) {
+        //                 //New item. We add something.
+        //                 state[i] = initialAdd;
+        //             } else if (selectionData[i] > 0 && state[i] > 1 && subtracted < initialAdd) {
+        //                 //Existing item. We still have stuff we need to take out. We subtract.
+        //                 state[i]--;
+        //                 subtracted++;
+        //             }
+        //         }
+        //
+        //     } else {
+        //         //There's a clear dominant so we don't have to balance.
+        //         console.log("Full n-recipe with dominant. Shifting.");
+        //         let maxAltered = false;
+        //         for (let i = 0; i < selectionData.length; i++) {
+        //             if (selectionData[i] > 0 && state[i] == 0) {
+        //                 //New item. We add something.
+        //                 state[i] = initialAdd;
+        //             } else if (selectionData[i] > 0 && state[i]  == maxSelected && !maxAltered) {
+        //                 //Existing item. We still have stuff we need to take out. We subtract.
+        //                 state[i] -= initialAdd;
+        //                 maxAltered = true;
+        //             }
+        //         }
+        //     }
+
+        // } else {
+        //     //General case
+        //     console.log("General n-recipe.");
+        //     for (let i = 0; i < selectionData.length; i++) {
+        //         if (selectionData[i] > 0 && state[i] == 0) {
+        //             state[i] = 2;
+        //         } else if (selectionData[i] == 0 && state[i] > 0) {
+        //             state[i] = 0;
+        //         }
+        //     }
+
+        if (nowSelected == maxFlavors) {
+            action = "FreezeOptions";
+        }
+    } else if (nowSelected > maxFlavors) {
+        action = "FreezeOptions";
+        console.log("Too many flavors active! How did this happen?")
+    }
+
+    updateUI();
+
+    // if (capacity == 0) {
+    //     action = "FreezeOptions";
+    // }
+
+    var message = {
+        action: action,
+        selection: state
+    };
+    window.parent.postMessage(message,"*");
+    selected = nowSelected;
+}
+
+function updateUI() {
+
+    controls.remove();
+    controls = draw.group();
+
+    capacity = units;
+    for (let i = 0; i < state.length; i++) {
+        capacity -= state[i];
+    }
+
+    let messge = 0;
+    if (capacity > 0) {
+        helpText.text("You have ");
+        helpText.build(true);
+        helpText.tspan(""+capacity).fill('Green');
+        if (capacity ==1 ) {
+            helpText.tspan(" part left to add");
+        } else {
+            helpText.tspan(" parts left to add");
+        }
+        helpText.build(false);
+
+        // message = "You have "+capacity+" parts left to add"
+    } else {
+        message = "Reduce flavors to change the balance"
+        helpText.text(message);
+    }
+
+    const bottleSide = 600;
+    const bottleImLeft = 580;
+    const bottleImTop = -45;
+    const levelInc = 32;
+    const bottleBottom = (units*levelInc) + 98;
+    const bottleRectLeft = 766;
+    const bottleWidth = 178;
+    const iconMax = 160;
+    // const bottleRight = bottleRectLeft + bottleWidth;
+
+    bottleBack = controls.rect(450,550);
+    bottleBack.fill('#F7F7F7');
+    bottleBack.move(bottleImLeft+100,bottleImTop);
+
+    var level = 0;
+    var element = yPos-1;
+    // for (let i = 0; i < state.lengh; i++) {
+    for (let i = state.length-1; i >= 0; i--) {
+
+        if (state[i] > 0) {
+            let rectHeight = state[i]*levelInc;
+            let rectTop = bottleBottom-(rectHeight+level);
+
+            rect = controls.rect(bottleWidth,rectHeight-2);
+            // rect.fill('#ECF2F9');
+            rect.fill('#DCE2F9');
+            rect.move(bottleRectLeft,rectTop);
+
+            level += rectHeight;
+            element--;
+        }
+    }
+
+    let bottle = controls.image(assets+"border-bottle.png",550,550);
+    bottle.move(bottleImLeft,bottleImTop);
+
+    var level = 0;
+    var element = yPos-1;
+    for (let i = state.length-1; i >= 0; i--) {
+
+        if (state[i] > 0) {
+            let rectHeight = state[i]*levelInc;
+            let rectTop = bottleBottom-(rectHeight+level);
+
+            let imSide = Math.min(iconMax, rectHeight);
+            let imLeft = bottleRectLeft + (bottleWidth-imSide)/2;
+            let imTop = rectTop + (rectHeight - imSide)/2;
+            let im = controls.image(assets+optionData[i].image,imSide,imSide);
+            im.move(imLeft, imTop);
+
+            let labelText = controls.text(optionData[i].name);
+            let length = labelText.length();
+            let adjust = (imSide - length)/2;
+            labelText.font({family:'Helvetica',size:20})
+            labelText.fill("Gray");
+            labelText.move(imLeft+adjust+1,1+imTop+(imSide/2)-10);
+
+            let labelText2 = controls.text(optionData[i].name);
+            labelText2.font({family:'Helvetica',size:20})
+            labelText2.fill("White");
+            labelText2.move(imLeft+adjust,imTop+(imSide/2)-10);
+
+            level += rectHeight;
+            element--;
+        }
+    }
+
+    const rowTop = 60;
+    const rowHeight = 100;
+    const textWidth = 100;
+    const textTop = 36;
+    const textLeft = 170;
+    const iconTop = 0;
+    const iconLeft = 80;
+    const iconSide = 80;
+    const dotGap = 30;
+    const diameter = 24;
+    const sliderHeight = 14;
+    const dotLeft = 280;
+    const dotTop = 40;
+    const borderLeft = 100;
+    const borderExtra = 180;
+
+    var yPos = 0;
+    for (let i = 0; i < state.length; i++) {
+        if (state[i] > 0) {
+
+            let icon = controls.image(assets+optionData[i].image,iconSide,iconSide);
+            icon.move(iconLeft,yPos*rowHeight);
+
+            let text = controls.text(optionData[i].name).move(textLeft, textTop + yPos*rowHeight);
+            text.fill('Black');
+
+            for (let j = 0; j < units; j++) {
+
+                let rect;
+                // if (j == state[i] - 1) {
+
+                // rect = controls.rect(rowHeight, rowHeight);
+                rect = controls.circle(diameter);
+                // rect.stroke({color: 'Black', width: 2})
+
+                let rectLeft = dotLeft+j*dotGap;
+                let rectTop = dotTop + yPos*rowHeight;
+                let dotXAdjust = (dotGap - diameter)/2;
+                let dotYAdjust = -(diameter - sliderHeight)/2;
+
+                rect.move(rectLeft + dotXAdjust,rectTop + dotYAdjust);
+
+                // } else {
+                //     rect = controls.rect(dotGap,sliderHeight);
+                //     rect.move(dotLeft+j*dotGap,dotTop + yPos*rowHeight);
+                // }
+
+                let highlightOn = true;
+                if (j < state[i]) {
+                    rect.fill('#52009A');
+                    rect.click(function() {updateState(i,j+1)});
+                    rect.style('cursor', 'pointer');
+                } else if (j < state[i]+capacity){
+                    rect.fill('White');
+                    rect.stroke({color:'Silver',width:1});
+                    rect.click(function() {updateState(i,j+1)});
+                    rect.style('cursor', 'pointer');
+                } else {
+                    highlightOn = false;
+                    rect.fill('DimGray');
+                }
+
+                if (highlightOn) {
+                    let highlight = controls.circle(4);
+                    highlight.move(dotLeft+(j+0.5)*dotGap-2,dotTop+yPos*rowHeight+sliderHeight/2-2);
+                    // highlight.fill(optionData[i].color);
+                    highlight.fill('none');
+                    rect.mouseover(function() {highlight.animate(5).fill('Silver')});
+                    // rect.mouseout(function() {highlight.animate(5).fill(optionData[i].color)});
+                    rect.mouseout(function() {highlight.animate(5).fill('none')});
+                    highlight.click(function() {updateState(i,j+1)});
+                }
+            }
+
+            const partsTDown = 5;
+            let separatorRight = borderLeft + (units*dotGap) + borderExtra;
+
+            let partString = "/"+units+" Parts";
+            // if (state[i]>1) partString += "s";
+            let partsText = controls.text(state[i]+partString);
+            partsText.fill('#333333');
+            partsText.font({'size':20});
+            let partsTLeft = separatorRight - (partsText.length()+4);
+            partsText.move(partsTLeft,(yPos*rowHeight)+partsTDown);
+
+            const delTLeft = (units*dotGap) + 220;
+            const delTDown = 70;
+
+            let delText = controls.text("Remove");
+            delText.fill('Gray');
+            delText.font({'size':14});
+            delText.move(delTLeft,(yPos*rowHeight)+delTDown);
+
+            delText.click(function() {updateState(i,0)});
+            delText.style('cursor', 'pointer');
+            delText.mouseover(function() {delText.animate(5).fill('Black')});
+            delText.mouseout(function() {delText.animate(5).fill('Gray')});
+
+            let separator = controls.line(
+                borderLeft,
+                (yPos+1)*rowHeight,
+                separatorRight,
+                (yPos+1)*rowHeight);
+            separator.stroke({color:'Silver', width:1});
+
+            yPos++;
+        }
+    }
+
+    if (capacity == units) {
+
+        helpText.text("");
+
+        let startText1 = controls.text("");
+        startText1.font({family:'Helvetica',size:20});
+        startText1.build(true);
+        startText1.tspan("Get started").fill('#52009A').font({weight:'bold'});
+        startText1.tspan(" by selecting some flavorings");
+        startText1.build(false);
+        startText1.move(120,140);
+
+        let startText2 = controls.text("for your vodka above...")
+        startText2.font({family:'Helvetica',size:20});
+        startText2.move(200,170);
+
+        let startIcon = controls.image(assets+"/mix.png",100,100);
+        startIcon.move(240,200);
+
+        let arrowIcon = controls.image(assets+"/arrow.png",160,160);
+        arrowIcon.move(360,-30);
+    }
+    controls.move(0,rowTop);
+}
+
+function updateState(i, j) {
+    console.log("updateState i:"+i+" j:"+j);
+    state[i] = j;
+
+    let nowSelected = 0;
+    for (let i = 0; i < state.length; i++) {
+        if (state[i] > 0) nowSelected++;
+    }
+
+    updateUI();
+
+    let action = "UpdateRecipe";
+    // if (capacity == 0) {
+    //     action = "FreezeOptions";
+    // } else
+    if (nowSelected >= maxFlavors) {
+        action = "FreezeOptions";
+    } else
+    if (j == 0) {
+        action = "RemoveIngredient";
+    }
+
+    var message = {
+        action: action,
+        selection: state
+    };
+    window.parent.postMessage(message,"*");
+}
