ce16c448fc45db7aa6d80431a3e023fe1f10a117
Alex Lamb <alex.lamb@gmail.com>
2017-12-14 15:10:09 -0800

    Dunkable ostrich bites with savory hands.

--- js/dull1.js
+++ js/dull1.js
@@ -1,11 +1,14 @@
 window.onload = function() {
-	let div = document.getElementById('svgCanvas');
+	let div = document.getElementById('check');
 	div.innerHTML += " Onload triggered.";
     div.innerHTML += dullData;
+
+    draw = SVG('svgCanvas').size(50,50);
+    draw.rect(50,50).fill("#987654");
 };
 
 window.onmessage = (event) => {
-	let div = document.getElementById('svgCanvas');
+	let div = document.getElementById('check');
 	div.innerHTML += " Onmessage triggered.";
 };
 
