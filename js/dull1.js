c642966ee917210c6467fffc79f2e22930769b5d
Alex Lamb <alex.lamb@gmail.com>
2017-12-14 15:22:22 -0800

    Telescopic pleausre brackets for mountable funjoy

--- js/dull1.js
+++ js/dull1.js
@@ -1,15 +1,21 @@
+
 window.onload = function() {
+    console.log("onload");
 	let div = document.getElementById('check');
 	div.innerHTML += " Onload triggered.";
     div.innerHTML += dullData;
 
+    console.log("drawing");
     draw = SVG('svgCanvas').size(50,50);
     draw.rect(50,50).fill("#987654");
 };
 
 window.onmessage = (event) => {
+    console.log("onmessage");
 	let div = document.getElementById('check');
 	div.innerHTML += " Onmessage triggered.";
+    console.log("redrawing");
+    draw.rect(50,50).fill("#456789");
 };
 
 function button_click() {
